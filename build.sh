9b91a9133d71767e626d7d5b9475c32421a573b2
Hunter Long <info@socialeck.com>
2018-06-15 09:14:22 -0700

    updates

--- build.sh
+++ build.sh
@@ -13,7 +13,6 @@ xgo --targets=linux/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 xgo --targets=linux/386 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 
 xgo --targets=windows/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
-xgo --targets=windows/386 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 
 
 cd build
@@ -25,4 +24,3 @@ mv build/$APP-darwin-10.6-386 build/$APP-osx-x32
 mv build/$APP-linux-amd64 build/$APP-linux-x64
 mv build/$APP-linux-386 build/$APP-linux-x32
 mv build/$APP-windows-4.0-amd64.exe build/$APP-windows-x64.exe
-mv build/$APP-windows-4.0-386.exe build/$APP-windows-x32.exe
