cf45a743987402c3567754f4acb8321a97948a84
Hunter Long <Info@socialeck.com>
2018-06-23 20:07:01 -0700

    arm7 and arm64

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-APP="statup"
 
-printf "\nUPDATE core SET version='$VERSION';" >> sql/upgrade.sql
+APP="statup"
+REPO="hunterlong/statup"
 
 rice embed-go
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build -f Dockerfile -t hunterlong/statup:$VERSION .
-docker tag hunterlong/statup:$VERSION hunterlong/statup:latest
-docker tag hunterlong/statup:$VERSION hunterlong/statup:$VERSION
-docker push hunterlong/statup
+docker build -f Dockerfile -t $REPO:$VERSION .
+docker tag $REPO:$VERSION $REPO:latest
+docker tag $REPO:$VERSION $REPO:$VERSION
+docker push $REPO
 
 mkdir build
 xgo -go 1.10.x --targets=darwin/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 xgo -go 1.10.x --targets=darwin/386 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 xgo -go 1.10.x --targets=linux/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 xgo -go 1.10.x --targets=linux/386 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
-xgo -go 1.10.x --targets=windows/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
+xgo -go 1.10.x --targets=windows-6.0/amd64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
+xgo -go 1.10.x --targets=linux/arm-7 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
+xgo -go 1.10.x --targets=linux/arm64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 
 cd build
 ls
@@ -26,4 +28,5 @@ mv build/$APP-darwin-10.6-amd64 build/$APP-osx-x64
 mv build/$APP-darwin-10.6-386 build/$APP-osx-x32
 mv build/$APP-linux-amd64 build/$APP-linux-x64
 mv build/$APP-linux-386 build/$APP-linux-x32
-mv build/$APP-windows-4.0-amd64.exe build/$APP-windows-x64.exe
+mv build/$APP-windows-6.0-amd64.exe build/$APP-windows-x64.exe
+mv build/$APP-linux-arm-7 build/$APP-linux-arm7
