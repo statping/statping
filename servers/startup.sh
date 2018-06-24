cc13bbfb556c98cf2dc73e612884bf727fb341e7
Hunter Long <Info@socialeck.com>
2018-06-24 15:54:37 -0700

    building

--- servers/startup.sh
+++ servers/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /home/ubuntu
-rm -f docker-compose.yml
-curl -o docker-compose.yml -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose.yml
+sudo rm -f docker-compose.yml
+sudo curl -o docker-compose.yml -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose.yml
 sudo service docker start
 sudo docker system prune -af
 sudo LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL docker-compose pull
