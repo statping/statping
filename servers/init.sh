abe05f7f3b490459fa2e64d59a3099fa57614030
Hunter Long <Info@socialeck.com>
2018-06-24 16:12:35 -0700

    building

--- servers/init.sh
+++ servers/init.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 cd /home/ubuntu
-curl -o /home/ubuntu/startup.sh -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/startup.sh
-chmod +x /home/ubuntu/startup.sh
-LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL ./startup.sh
+sudo rm -rf startup.sh
+sudo curl -o startup.sh -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/startup.sh
+sudo chmod +x startup.sh
+
+sudo rm -f docker-compose.yml
+if [ "$LETSENCRYPT_HOST" = "" ]
+then
+   sudo curl -o docker-compose.yml -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose.yml
+else
+   sudo curl -o docker-compose.yml -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose-ssl.yml
+fi
+
+sudo service docker start
+sudo docker system prune -af
+
+if [ "$LETSENCRYPT_HOST" = "" ]
+then
+   sudo docker-compose pull
+   sudo docker-compose up -d
+else
+    sudo LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL docker-compose pull
+    sudo LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL docker-compose up -d
+fi
+
+
--- servers/startup-ssl.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd /home/ubuntu
-rm -f docker-compose.yml
-curl -o docker-compose.yml -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose-ssl.yml
-sudo service docker start
-sudo docker system prune -af
-sudo LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL docker-compose pull
-sudo LETSENCRYPT_HOST=$LETSENCRYPT_HOST LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL docker-compose up -d
