4cd56087fd1aa12eb8c80c06afd3eba93e2de7ef
Hunter Long <Info@socialeck.com>
2018-06-23 20:33:08 -0700

    auto ssl

---
+++ servers/ubuntu-ssl.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+DOMAIN="status.balancebadge.io"
+EMAIL="info@socialeck.com"
+
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
+sudo systemctl enable docker
+mkdir statup
+cd statup
+wget https://raw.githubusercontent.com/hunterlong/statup/master/servers/docker-compose-ssl.yml
+sudo service docker start
+sudo docker-compose up -d
