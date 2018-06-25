548cf02c1fb4f07b5555e83c54bbc81ea1912277
Hunter Long <Info@socialeck.com>
2018-06-25 02:25:25 -0700

    building

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM alpine:latest
 
-ENV VERSION=v0.23
+ENV VERSION=v0.241
 
 RUN apk --no-cache add ca-certificates
 RUN wget https://github.com/hunterlong/statup/releases/download/$VERSION/statup-linux-alpine.tar.gz && \
       tar -xvzf statup-linux-alpine.tar.gz && \
+      ls && \
       chmod +x statup && \
       mv statup /usr/local/bin/statup
 WORKDIR /app
--- build.sh
+++ build.sh
@@ -16,10 +16,13 @@ xgo -go 1.10.x --targets=windows-6.0/amd64 --dest=build -ldflags="-X main.VERSIO
 xgo -go 1.10.x --targets=linux/arm-7 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 xgo -go 1.10.x --targets=linux/arm64 --dest=build -ldflags="-X main.VERSION=$VERSION" ./
 
-CGO_ENABLED=0 GOOS=linux go build -ldflags="-X main.VERSION=$VERSION" -a -o build/statup-linux-alpine .
+CGO_ENABLED=0 GOOS=linux go build -ldflags="-X main.VERSION=$VERSION" -a -o build/$APP-linux-alpine .
 
 cd build
 ls
+
+mv $APP-linux-alpine $APP
+tar -czvf $APP-linux-alpine.tar.gz $APP && rm -f $APP
 
 mv $APP-darwin-10.6-amd64 $APP
 tar -czvf $APP-osx-x64.tar.gz $APP && rm -f $APP
@@ -41,9 +44,6 @@ tar -czvf $APP-linux-arm7.tar.gz $APP && rm -f $APP
 
 mv $APP-linux-arm64 $APP
 tar -czvf $APP-linux-arm64.tar.gz $APP && rm -f $APP
-
-mv $APP-linux-alpine build/$APP
-tar -czvf $APP-linux-alpine.tar.gz $APP && rm -f $APP
 
 body='{
  "request": {
