dbb63ea13554df65b384f9b4dab6b8e754477325
Hunter Long <Info@socialeck.com>
2018-07-19 23:37:37 -0700

    database migration fix

--- source/sql/sqlite_upgrade.sql
+++ source/sql/sqlite_upgrade.sql
@@ -1,5 +1,5 @@
-=========================================== 1531891670
-ALTER TABLE services ALTER COLUMN order_id SET DEFAULT 0;
+=========================================== 1532068515
+ALTER TABLE services ALTER COLUMN order_id integer DEFAULT 0;
 ALTER TABLE services ADD COLUMN timeout integer DEFAULT 30;
 =========================================== 1530841150
 ALTER TABLE core ADD COLUMN use_cdn bool DEFAULT FALSE;
