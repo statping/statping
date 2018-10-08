3d6daf0b9009aa5ebc108cef77e0d2e2312fda9c
Hunter Long <info@socialeck.com>
2018-10-07 21:37:23 -0700

    testing - comments

--- utils/doc.go
+++ utils/doc.go
@@ -1,7 +1,7 @@
 // Package utils contains common methods used in most packages in Statup.
 // This package contains multiple function like:
 // Logging, encryption, type conversions, setting utils.Directory as the current directory,
-// running local CMD commands, and creaing/deleting files/folder.
+// running local CMD commands, and creating/deleting files/folder.
 //
 // You can overwrite the utils.Directory global variable by including
 // STATUP_DIR environment variable to be an absolute path.
