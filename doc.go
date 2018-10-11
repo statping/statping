546e29b41226a2d474703a054b531680ec968366
hunterlong <info@socialeck.com>
2018-10-10 19:53:19 -0700

    doc

--- doc.go
+++ doc.go
@@ -1,2 +1,24 @@
-// Package statup is a server monitoring application that includs a status page server.
+// Package statup is a server monitoring application that includs a status page server. Visit the Statup repo at
+// https://github.com/hunterlong/statup to get a full understanding of what this application can do.
+//
+// Install Statup
+//
+// Statup is available for Mac, Linux and Windows 64x. You can download the tar.gz file of use a couple other methods. Download
+// the latest release at https://github.com/hunterlong/statup/releases/latest or use the methods below. If you're on windows, download
+// the zip file from the latest releases link.
+//
+//		// MacOS using homebrew
+//		brew tap hunterlong/statup
+//		brew install statup
+//
+//		// Linux installation
+//		bash <(curl -s https://assets.statup.io/install.sh)
+//		statup version
+//
+// Docker
+//
+// Statup can be built in many way, the best way is to use Docker!
+//		docker run -it -p 8080:8080 hunterlong/statup
+//
+// Enjoy Statup and tell me any issues you might be having on Github. https://github.com/hunterlong
 package statup
