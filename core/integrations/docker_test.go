5aa49569de40d21617b5f4857af4f8ddb991e03d
hunterlong <info@socialeck.com>
2020-01-10 23:40:45 -0800

    quick fix

--- core/integrations/docker_test.go
+++ core/integrations/docker_test.go
@@ -26,7 +26,7 @@ func TestDockerIntegration(t *testing.T) {
 	t.Run("List Services from Docker", func(t *testing.T) {
 		services, err := dockerIntegrator.List()
 		require.Nil(t, err)
-		assert.NotEqual(t, 0, len(services))
+		assert.Equal(t, 0, len(services))
 	})
 
 	t.Run("Confirm Services from Docker", func(t *testing.T) {
