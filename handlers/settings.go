80dd10511e870052af8d5c16441716c6c21c5204
Emanuel Bennici <eb@fabmation.de>
2020-02-17 14:18:48 +0100

    Add 'data-retention' value to settings handler

--- handlers/settings.go
+++ handlers/settings.go
@@ -64,6 +64,12 @@ func saveSettingsHandler(w http.ResponseWriter, r *http.Request) {
 	timeFloat, _ := strconv.ParseFloat(timezone, 10)
 	app.Timezone = float32(timeFloat)
 
+	app.DataRetention, err = strconv.ParseInt(form.Get("data-retention"),
+		10, 64)
+	if err != nil {
+		log.Errorf("Can not convert form data to int64: %v\n", err.Error())
+	}
+
 	app.UpdateNotify = types.NewNullBool(form.Get("update_notify") == "true")
 
 	app.UseCdn = types.NewNullBool(form.Get("enable_cdn") == "on")
