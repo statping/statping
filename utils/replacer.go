1053be44b0562deaa434b9be68b9c928f46e87e7
Emanuel Bennici <eb@fabmation.de>
2020-04-27 13:34:31 +0200

    Fix email template
    The Email notifier was using `map[string]interface{}` to "put" the
    information (like Service and Failure struct) into the template.
    The value of a map can only be used with `{{ index . "Service" }}` in a
    GoLang template.
    To simplify – and reduce the needed changes – `emailOutgoing` now
    contains for the `Data` field an extra struct with `Service` and
    `Failure` fields.
    
    This patch fixes #472.

--- utils/replacer.go
+++ utils/replacer.go
@@ -2,32 +2,23 @@ package utils
 
 import (
 	"bytes"
-	"fmt"
 	"text/template"
 )
 
 func ReplaceTemplate(tmpl string, data interface{}) string {
 	buf := new(bytes.Buffer)
-	var varStr string
-	switch fmt.Sprintf("%T", data) {
-	case "*services.Service":
-		varStr = "Service"
-	case "*failures.Failure":
-		varStr = "Failure"
-	default:
-		varStr = "Object"
-	}
-	injectVars := make(map[string]interface{})
-	injectVars[varStr] = data
+
 	slackTemp, err := template.New("replacement").Parse(tmpl)
 	if err != nil {
 		Log.Error(err)
 		return err.Error()
 	}
-	err = slackTemp.Execute(buf, injectVars)
+
+	err = slackTemp.Execute(buf, data)
 	if err != nil {
 		Log.Error(err)
 		return err.Error()
 	}
+
 	return buf.String()
 }
