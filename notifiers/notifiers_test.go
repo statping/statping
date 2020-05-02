8ac6fff7bfd3409e3efbe172b392cca21ac71593
hunterlong <info@socialeck.com>
2020-05-02 04:06:02 -0700

    version up, is today a tuesday?

--- notifiers/notifiers_test.go
+++ notifiers/notifiers_test.go
@@ -12,5 +12,5 @@ func TestReplaceTemplate(t *testing.T) {
 
 	temp = `{"id":{{.Service.Id}},"name":"{{.Service.Name}}","downtime":"{{.Service.DowntimeAgo}}","failure":"{{.Failure.Issue}}"}`
 	replaced = ReplaceTemplate(temp, replacer{Service: exampleService, Failure: exampleFailure})
-	assert.Equal(t, `{"id":1,"name":"Statping","failure":"HTTP returned a 500 status code"} want {"id":1,"name":"Statping","downtime":"15 seconds ago","failure":"HTTP returned a 500 status code"}`, replaced)
+	assert.Equal(t, `{"id":1,"name":"Statping","downtime":"15 seconds ago","failure":"HTTP returned a 500 status code"}`, replaced)
 }
