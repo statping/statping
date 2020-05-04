2aa3bee6238a9a8e90e0e594cfdfef5206813680
hunterlong <info@socialeck.com>
2020-05-03 17:33:26 -0700

    test fix

--- notifiers/notifiers_test.go
+++ notifiers/notifiers_test.go
@@ -10,7 +10,7 @@ func TestReplaceTemplate(t *testing.T) {
 	replaced := ReplaceTemplate(temp, replacer{Service: exampleService})
 	assert.Equal(t, `{"id":1,"name":"Statping"}`, replaced)
 
-	temp = `{"id":{{.Service.Id}},"name":"{{.Service.Name}}","downtime":"{{.Service.DowntimeAgo}}","failure":"{{.Failure.Issue}}"}`
+	temp = `{"id":{{.Service.Id}},"name":"{{.Service.Name}}","failure":"{{.Failure.Issue}}"}`
 	replaced = ReplaceTemplate(temp, replacer{Service: exampleService, Failure: exampleFailure})
-	assert.Equal(t, `{"id":1,"name":"Statping","downtime":"15 seconds ago","failure":"HTTP returned a 500 status code"}`, replaced)
+	assert.Equal(t, `{"id":1,"name":"Statping","failure":"HTTP returned a 500 status code"}`, replaced)
 }
