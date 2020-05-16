5c15c203924b93d8c73ffc1705300cc9f4155642
Kevin Petit <Kevin.Petit@equadex.net>
2020-05-16 18:47:44 +0200

    Reformat code

--- frontend/src/components/Dashboard/DashboardMessages.vue
+++ frontend/src/components/Dashboard/DashboardMessages.vue
@@ -1,40 +1,37 @@
 <template>
-    <div class="col-12">
-        <div class="card contain-card text-black-50 bg-white mb-4">
-            <div class="card-header">Announcements</div>
-            <div class="card-body pt-0">
+  <div class="col-12">
+    <div class="card contain-card text-black-50 bg-white mb-4">
+      <div class="card-header">Announcements</div>
+      <div class="card-body pt-0">
         <table class="table table-striped">
-            <thead>
+          <thead>
             <tr>
                 <th scope="col">Title</th>
                 <th scope="col" class="d-none d-md-table-cell">Service</th>
                 <th scope="col" class="d-none d-md-table-cell">Begins</th>
                 <th scope="col"></th>
             </tr>
-            </thead>
-            <tbody>
-
+          </thead>
+          <tbody>
             <tr v-for="message in $store.getters.messages" v-bind:key="message.id">
-                <td>{{message.title}}</td>
-                <td class="d-none d-md-table-cell">
-                  <router-link :to="serviceLink(service(message.service))">{{serviceName(service(message.service))}}</router-link>
-                </td>
-                <td class="d-none d-md-table-cell">{{niceDate(message.start_on)}}</td>
-                <td class="text-right">
-                    <div v-if="$store.state.admin" class="btn-group">
-                        <a @click.prevent="editMessage(message, edit)" href="#" class="btn btn-outline-secondary"><i class="fas fa-exclamation-triangle"></i> Edit</a>
-                        <a @click.prevent="deleteMessage(message)" href="#" class="btn btn-danger"><font-awesome-icon icon="times" /></a>
-                    </div>
-                </td>
+              <td>{{message.title}}</td>
+              <td class="d-none d-md-table-cell">
+                <router-link :to="serviceLink(service(message.service))">{{serviceName(service(message.service))}}</router-link>
+              </td>
+              <td class="d-none d-md-table-cell">{{niceDate(message.start_on)}}</td>
+              <td class="text-right">
+                <div v-if="$store.state.admin" class="btn-group">
+                  <a @click.prevent="editMessage(message, edit)" href="#" class="btn btn-outline-secondary"><i class="fas fa-exclamation-triangle"></i> Edit</a>
+                  <a @click.prevent="deleteMessage(message)" href="#" class="btn btn-danger"><font-awesome-icon icon="times" /></a>
+                </div>
+              </td>
             </tr>
-
-            </tbody>
+          </tbody>
         </table>
+      </div>
     </div>
-        </div>
-
-            <FormMessage v-if="$store.state.admin" :edit="editChange" :in_message="message"/>
-    </div>
+    <FormMessage v-if="$store.state.admin" :edit="editChange" :in_message="message"/>
+  </div>
 </template>
 
 <script>
