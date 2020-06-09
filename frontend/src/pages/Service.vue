e04e09872e4b7ddc2cd03a4bebeec0fa18f00922
Patrick Fruh <809734+kaeltis@users.noreply.github.com>
2020-06-09 18:35:08 +0200

    fix time conversion in Service.vue

--- frontend/src/pages/Service.vue
+++ frontend/src/pages/Service.vue
@@ -270,9 +270,9 @@ export default {
                         const dt = new Date(ts).toLocaleDateString("en-us", timeoptions)
                         let val = series[seriesIndex][dataPointIndex];
                         if (val >= 1000) {
-                            val = (val * 0.1).toFixed(0) + " milliseconds"
+                            val = (val / 1000).toFixed(0) + " milliseconds"
                         } else {
-                            val = (val * 0.01).toFixed(0) + " microseconds"
+                            val = (val).toFixed(0) + " microseconds"
                         }
                         return `<div class="chartmarker"><span>Response Time: </span><span class="font-3">${val}</span><span>${dt}</span></div>`
                     },
