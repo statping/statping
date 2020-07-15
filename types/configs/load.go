7120015c7c936a2ecb24e6b3de0db70d0cbd9135
hunterlong <info@socialeck.com>
2020-07-15 10:09:00 -0700

    letsencrypt enable ENV var

--- types/configs/load.go
+++ types/configs/load.go
@@ -57,29 +57,43 @@ func LoadConfigs(cfgFile string) (*DbConfig, error) {
 		p.Set("API_SECRET", db.ApiSecret)
 	}
 	if db.Language != "" {
-		p.Set("LANGUAGE", "en")
+		p.Set("LANGUAGE", db.Language)
 	}
 	if db.SendReports {
 		p.Set("ALLOW_REPORTS", true)
 	}
+	if db.LetsEncryptEmail != "" {
+		p.Set("LETSENCRYPT_EMAIL", db.LetsEncryptEmail)
+	}
+	if db.LetsEncryptHost != "" {
+		p.Set("LETSENCRYPT_HOST", db.LetsEncryptHost)
+	}
+	if db.LetsEncryptEnable {
+		p.Set("LETSENCRYPT_ENABLE", db.LetsEncryptEnable)
+	}
 
 	configs := &DbConfig{
-		DbConn:      p.GetString("DB_CONN"),
-		DbHost:      p.GetString("DB_HOST"),
-		DbUser:      p.GetString("DB_USER"),
-		DbPass:      p.GetString("DB_PASS"),
-		DbData:      p.GetString("DB_DATABASE"),
-		DbPort:      p.GetInt("DB_PORT"),
-		Project:     p.GetString("NAME"),
-		Description: p.GetString("DESCRIPTION"),
-		Domain:      p.GetString("DOMAIN"),
-		Email:       p.GetString("EMAIL"),
-		Username:    p.GetString("ADMIN_USER"),
-		Password:    p.GetString("ADMIN_PASSWORD"),
-		Location:    utils.Directory,
-		SqlFile:     p.GetString("SQL_FILE"),
-		Language:    p.GetString("LANGUAGE"),
-		SendReports: p.GetBool("ALLOW_REPORTS"),
+		DbConn:            p.GetString("DB_CONN"),
+		DbHost:            p.GetString("DB_HOST"),
+		DbUser:            p.GetString("DB_USER"),
+		DbPass:            p.GetString("DB_PASS"),
+		DbData:            p.GetString("DB_DATABASE"),
+		DbPort:            p.GetInt("DB_PORT"),
+		Project:           p.GetString("NAME"),
+		Description:       p.GetString("DESCRIPTION"),
+		Domain:            p.GetString("DOMAIN"),
+		Email:             p.GetString("EMAIL"),
+		Username:          p.GetString("ADMIN_USER"),
+		Password:          p.GetString("ADMIN_PASSWORD"),
+		Location:          utils.Directory,
+		SqlFile:           p.GetString("SQL_FILE"),
+		Language:          p.GetString("LANGUAGE"),
+		SendReports:       p.GetBool("ALLOW_REPORTS"),
+		LetsEncryptEnable: p.GetBool("LETSENCRYPT_ENABLE"),
+	}
+	if configs.LetsEncryptEnable {
+		configs.LetsEncryptHost = p.GetString("LETSENCRYPT_HOST")
+		configs.LetsEncryptEmail = p.GetString("LETSENCRYPT_EMAIL")
 	}
 	log.WithFields(utils.ToFields(configs)).Debugln("read config file: " + cfgFile)
 
--- types/configs/struct.go
+++ types/configs/struct.go
@@ -6,26 +6,29 @@ const SqliteFilename = "statping.db"
 
 // DbConfig struct is used for the Db connection and creates the 'config.yml' file
 type DbConfig struct {
-	DbConn      string `yaml:"connection" json:"connection"`
-	DbHost      string `yaml:"host" json:"-"`
-	DbUser      string `yaml:"user" json:"-"`
-	DbPass      string `yaml:"password" json:"-"`
-	DbData      string `yaml:"database" json:"-"`
-	DbPort      int    `yaml:"port" json:"-"`
-	ApiSecret   string `yaml:"api_secret" json:"-"`
-	Language    string `yaml:"language" json:"language"`
-	SendReports bool   `yaml:"send_reports" json:"send_reports"`
-	Project     string `yaml:"-" json:"-"`
-	Description string `yaml:"-" json:"-"`
-	Domain      string `yaml:"-" json:"-"`
-	Username    string `yaml:"-" json:"-"`
-	Password    string `yaml:"-" json:"-"`
-	Email       string `yaml:"-" json:"-"`
-	Error       error  `yaml:"-" json:"-"`
-	Location    string `yaml:"location" json:"-"`
-	SqlFile     string `yaml:"sqlfile,omitempty" json:"-"`
-	LocalIP     string `yaml:"-" json:"-"`
-	filename    string `yaml:"-" json:"-"`
+	DbConn            string `yaml:"connection" json:"connection"`
+	DbHost            string `yaml:"host" json:"-"`
+	DbUser            string `yaml:"user" json:"-"`
+	DbPass            string `yaml:"password" json:"-"`
+	DbData            string `yaml:"database" json:"-"`
+	DbPort            int    `yaml:"port" json:"-"`
+	ApiSecret         string `yaml:"api_secret" json:"-"`
+	Language          string `yaml:"language" json:"language"`
+	SendReports       bool   `yaml:"send_reports" json:"send_reports"`
+	Project           string `yaml:"-" json:"-"`
+	Description       string `yaml:"-" json:"-"`
+	Domain            string `yaml:"-" json:"-"`
+	Username          string `yaml:"-" json:"-"`
+	Password          string `yaml:"-" json:"-"`
+	Email             string `yaml:"-" json:"-"`
+	Error             error  `yaml:"-" json:"-"`
+	Location          string `yaml:"location" json:"-"`
+	SqlFile           string `yaml:"sqlfile,omitempty" json:"-"`
+	LetsEncryptHost   string `yaml:"letsencrypt_host,omitempty" json:"letsencrypt_host"`
+	LetsEncryptEmail  string `yaml:"letsencrypt_email,omitempty" json:"letsencrypt_email"`
+	LetsEncryptEnable bool   `yaml:"letsencrypt_enable" json:"letsencrypt_enable"`
+	LocalIP           string `yaml:"-" json:"-"`
+	filename          string `yaml:"-" json:"-"`
 
 	Db database.Database `yaml:"-" json:"-"`
 }
