fa09889e236b62375fd3b46bdf07aed380ea91f0
hunterlong <info@socialeck.com>
2020-08-12 21:45:17 -0700

    fixed global messages for services

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -203,9 +203,9 @@ router.beforeEach((to, from, next) => {
   const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
   const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
   const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
-  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+  if (nearestWithTitle) document.title = nearestWithTitle.meta.title;
   Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode.removeChild(el));
-  if(!nearestWithMeta) return next();
+  if (!nearestWithMeta) return next();
   nearestWithMeta.meta.metaTags.map(tagDef => {
     const tag = document.createElement('meta');
     Object.keys(tagDef).forEach(key => {
