2319cd36d1a5bacc216109e36857338c7a5a4ac7
hunterlong <info@socialeck.com>
2020-08-19 13:06:57 -0700

    A+ SSL score

--- handlers/server.go
+++ handlers/server.go
@@ -94,9 +94,9 @@ func startSSLServer(ip string) {
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
 		},
 	}
 	srv := &http.Server{
