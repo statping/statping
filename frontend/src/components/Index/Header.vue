61cdeaebee2a8e41af5f48530d0e2d554174ed0b
Adam <adam@aboutcher.co.uk>
2021-07-09 17:42:55 +0100

    Updated header.vue to div header div an id
    This is to make CSS style targeting easier.
    
    could be a div id=header or header element, I decided on the id so it shouldn't cause issues with any previous theming.

--- frontend/src/components/Index/Header.vue
+++ frontend/src/components/Index/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="header">
         <h1 id="title" class="col-12 text-center pt-4 mt-4 mb-3 header-title font-6">{{core.name}}</h1>
         <h5 id="description" class="col-12 text-center mb-5 header-desc font-3">{{core.description}}</h5>
     </div>
