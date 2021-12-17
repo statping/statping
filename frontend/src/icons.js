051bc3fe994cf5f0f40b3d8aef1b167760d17417
smit95tpatel <smit.patel@razorpay.com>
2021-12-17 19:25:38 +0530

    removed linting from icon

--- frontend/src/icons.js
+++ frontend/src/icons.js
@@ -1,9 +1,9 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/fontawesome-free-solid';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import Vue from 'vue';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/fontawesome-free-solid';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import Vue from "vue";
 
-library.add(fas, fab);
+library.add(fas, fab)
 
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
