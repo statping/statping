13f7cd3b70ae97a6fc48ebfefa00606435a12ee1
Rhythm <35167328+kRhythm@users.noreply.github.com>
2021-12-20 03:45:39 +0530

    added time contraint

--- types/downtimes/database.go
+++ types/downtimes/database.go
@@ -58,6 +58,19 @@ func FindByService(service int64, start time.Time, end time.Time) (*[]Downtime, 
 	return &downtime, q.Error()
 }
 
+func FindStatusByTime(service int64, timeVar time.Time) (string) {
+	var downtime []Downtime
+	q := db.Where("service = $1 and start BETWEEN $2 AND $3 ", service, time.Time{}, timeVar)
+	q = q.Order("id ASC ").Find(&downtime)
+	downtimeList := *(&downtime)
+	for _,dtime := range downtimeList{
+		if (*(dtime.End)).Unix() > timeVar.Unix(){
+			return dtime.SubStatus
+		}
+	}
+	return ""
+}
+
 func (c *Downtime) Create() error {
 	q := db.Create(c)
 	return q.Error()
