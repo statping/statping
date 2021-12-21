593194b8e3101fc90e2be44fdbc4318a1946f254
smit95tpatel <smit.patel@razorpay.com>
2021-12-21 16:24:56 +0530

    removed linting issues

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,28 +1,29 @@
 module.exports = {
-    assetsDir: 'assets',
-    filenameHashing: false,
-    productionTip: process.env.NODE_ENV !== 'production',
-    devtools: process.env.NODE_ENV !== 'production',
-    performance: process.env.NODE_ENV !== 'production',
-    devServer: {
-      disableHostCheck: true,
-      proxyTable: {
-        '/api': {
-          logLevel: 'debug',
-          target: 'http://0.0.0.0:8585',
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api': ''
-          }
-        },
-        '/oauth': {
-          logLevel: 'debug',
-          target: 'http://0.0.0.0:8585',
-          changeOrigin: true,
-          pathRewrite: {
-            '^/oauth': ''
-          }
+  baseUrl: '/',
+  assetsDir: 'assets',
+  filenameHashing: false,
+  productionTip: process.env.NODE_ENV !== 'production',
+  devtools: process.env.NODE_ENV !== 'production',
+  performance: process.env.NODE_ENV !== 'production',
+  devServer: {
+    disableHostCheck: true,
+    proxyTable: {
+      '/api': {
+        logLevel: 'debug',
+        target: 'http://0.0.0.0:8585',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/oauth': {
+        logLevel: 'debug',
+        target: 'http://0.0.0.0:8585',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/oauth': ''
         }
       }
     }
+  }
 };
