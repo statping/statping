ec731ee1515c198cd10c5095e09fb7964941510f
smit95tpatel <smit.patel@razorpay.com>
2022-05-16 14:29:07 +0530

    fix: linting

--- react-frontend/src/utils/trackers.js
+++ react-frontend/src/utils/trackers.js
@@ -2,19 +2,19 @@ import analyticsService from '@razorpay/universe-utils/analytics';
 import { getUserId } from './helper';
 
 export const initLumberjack = () => {
-	analyticsService.init({
-		lumberjackAppName: 'external_status_page',
-		lumberjackApiKey: window.LUMBERJACK_API_KEY,
-		lumberjackApiUrl: window.LUMBERJACK_API_URL,
-	});
+  analyticsService.init({
+    lumberjackAppName: 'external_status_page',
+    lumberjackApiKey: window.LUMBERJACK_API_KEY,
+    lumberjackApiUrl: window.LUMBERJACK_API_URL, 
+  });
 };
 
 export const analyticsTrack = (trackObj) => {
-	analyticsService.track({
-		...trackObj,
-		properties: {
-			...trackObj.properties,
-			userId: getUserId(),
-		},
-	});
+  analyticsService.track({
+    ...trackObj,
+    properties: {
+      ...trackObj.properties,
+      userId: getUserId(),
+    },
+  });
 };
